3bcc15a83881cbbbd876ac62d8d18ed45761967d
Paul <paul.iand3138@hotmail.com>
2022-11-11 19:17:01 +0800

    Use dynamic data in ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem() {
-  return (
-    <div className="expense-item">
-      <div>11th November 2022</div>
-      <div className="expense-item__description">
-        <h2>Pet Insurance</h2>
-        <div className="expense-item__price">RM 300</div>
-      </div>
-    </div>
-  );
+    const expenseDate = new Date(2022, 11, 11);
+    const expenseTitle = "Pet Insurance";
+    const expenseAmount = 300;
+
+    return (
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description">
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">RM {expenseAmount}</div>
+            </div>
+        </div>
+    );
 }
 
 export default ExpenseItem;
