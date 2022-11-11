8ccc078c2a920a1d257234e44b7fc19a278efdcd
Paul <paul.iand3138@hotmail.com>
2022-11-11 19:27:02 +0800

    Pass data into ExpenseItem component via props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
     const expenseDate = new Date(2022, 11, 11);
     const expenseTitle = "Pet Insurance";
     const expenseAmount = 300;
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">RM {expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">RM {props.amount}</div>
             </div>
         </div>
     );
